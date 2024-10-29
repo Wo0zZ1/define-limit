a40ca185196fe5c0098f73f35a7d84c2df79f149
Wo0zZ1 <Mper06@mail.ru>
2024-10-29 20:06:18 +0300

    Edit site name in Logo

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -9,7 +9,7 @@ const index = () => {
 				className='flex gap-5 rounded-xl px-5 py-3 bg-[#DE8DE8bd] hover:bg-[#DE8DE8]'>
 				<img src={Logo} alt='logo' />
 				<span className='text-3xl font-bold uppercase'>
-					Limit Solver
+					Define Limit
 				</span>
 			</Link>
 		</>
