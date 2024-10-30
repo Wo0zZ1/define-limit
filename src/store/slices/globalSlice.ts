bbd61f2fccb6dace06698c4679f73782cffbfbc7
Wo0zZ1 <Mper06@mail.ru>
2024-10-30 09:00:49 +0300

    temp

--- src/store/slices/globalSlice.ts
+++ src/store/slices/globalSlice.ts
@@ -1,6 +1,6 @@
+import { IDefinitionState } from './globalSlice'
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { Exception } from 'sass'
 
 export interface ILimitState {
 	from: string
@@ -32,12 +32,106 @@ const initialState: globalState = {
 	},
 }
 
+class LimitParser {
+	public func: string
+	public f: string
+	public arg: string
+	public infinity: Infinity
+	public equal: number
+
+	constructor(
+		func: string,
+		f: string,
+		arg: string,
+		infinity: Infinity,
+		equal: number,
+	) {
+		this.func = func
+		this.f = f
+		this.arg = arg
+		this.infinity = infinity
+		this.equal = equal
+	}
+
+	public getState = () => {
+    if this.infinity.
+    
+		return {
+			gamma: `arg ${this.infinity.sign}`,
+      eps: `|${func}${}|`
+		}
+	}
+}
+
+class Infinity {
+	private s: string
+	public sign: '+' | '-' | '' = ''
+	public num: '∞' | number = '∞'
+	public diff: string = ''
+
+	constructor(s: string) {
+		this.s = s
+		this.parseSign().parseNum()
+	}
+
+	private parseSign = (): Infinity => {
+		switch (this.s[0]) {
+			case '+':
+				this.sign = '+'
+				break
+			case '-':
+				this.sign = '-'
+				break
+			default:
+				this.sign = ''
+				break
+		}
+		return this
+	}
+
+	private parseNum = (): Infinity => {
+		const temp = this.s.substring(this.sign == '' ? 0 : 1)
+		if (temp.includes('∞')) {
+			this.num = '∞'
+			return this
+		}
+		const data = /(\d+)([+-]0)?/.exec(temp)?.splice(1, 3)
+		if (!data) return this
+		this.num = parseInt(data[0])
+		this.diff = data[1]
+		return this
+	}
+}
+
+const parseFunction = (s: string): undefined | string[] => {
+	const result = /([a-zA-Z])\(([a-zA-Z])\)/.exec(s)?.splice(1, 2)
+	if (!result || result.length !== 2) return
+	return result
+}
+
 const calculateDefinition = (
 	limitForm: ILimitState,
-): IDefinitionState => {
+): IDefinitionState | undefined => {
 	const { from, to, func, equal } = limitForm
-	console.log(from, to)
-	throw new Error('Function not completed')
+
+	// function and argument
+	const data = parseFunction(func)
+	if (!data) return
+	const [f, arg] = data
+	limitForm.from = arg
+	//
+
+	// to
+	const tmp: Infinity = new Infinity(to)
+	//
+
+	const lim: LimitParser = new LimitParser(
+		arg,
+		tmp,
+		parseInt(equal, 10),
+	)
+
+	return lim.getState()
 }
 
 const calculateLimit = (
